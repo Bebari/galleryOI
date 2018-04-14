0b0f98a6e24d34ddec4e9c3deb08bf84cdcda00b
bebari <rok.plevel@gmail.com>
2018-04-14 13:35:53 +0200

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var feed = new Instafeed({
   get: 'user',
-  userId: 'rok.krytor30',
-  clientId: 'eedb993f22274318a07a5e2bfb4c7d46'
+  userId: '7503766599',
+  clientId: 'eedb993f22274318a07a5e2bfb4c7d46' //'localhost: 9f5df136d1fb43ddb29558b6f8941f5f'
 });
 feed.run();
