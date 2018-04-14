b8304185b087b622ad68df963ad855715714ecc5
bebari <rok.plevel@gmail.com>
2018-04-14 13:21:26 +0200

    added script

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
     <script type="text/javascript" src="scripts/colorify.min.js"></script>
     <script type="text/javascript" src="scripts/instafeed.min.js"></script>
+    <script type="text/javascript" src="scripts/main.js"></script>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
